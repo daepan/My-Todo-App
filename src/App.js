2477ec886ae2fbab2500121b21fc69ca335eda2b
murpan <qw04011@gmail.com>
2021-09-17 15:54:23 +0900

    component optimization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
@@ -7,20 +7,43 @@ import './App.css'
 
 
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'TOEIC Study',
-      checked: false,
-    },
-    {
-      id: 2,
-      text: '1일 1커밋',
-      checked: false,
-    },
+function createBulkTodos() {
+  const array = [{
+    id: 1,
+    text: 'TOEIC Study',
+    checked: false,
+  },
+  {
+    id: 2,
+    text: '1일 1커밋',
+    checked: false,
+  },
 
-  ])
+  ];
+
+  return array;
+
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo)
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id)
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
+
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos)
+
 
   const NextId = useRef(4);
 
@@ -31,25 +54,21 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos => todos.concat(todo));
+      dispatch({ type: 'INSERT', todo })
       NextId.current += 1;
     }, [],
   )
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
+      dispatch({ type: 'REMOVE', id })
     },
     [],
   )
 
   const onToggle = useCallback(
     id => {
-      setTodos(todos =>
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        )
-      )
+      dispatch({ type: 'TOGGLE', id })
     }, []
   )
 
