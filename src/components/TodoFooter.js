cc5cff511291a54ca88c87bfce5e903816eb455a
김대관 <qw04011@gmail.com>
2021-09-24 14:54:04 +0900

    Update footer

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import './css/TodoFooter.scss';
+import styled from 'styled-components'
+
+
+
+const Main = styled.div`
+    display: flex;
+    font-family: 'Gluten-Bold';
+    font-size:32px;
+`;
+
 
 const TodoFooter =()=>{
     return (
         <div className="TodoFooter">
             <div className="Footer__Content">
-                <h2 className="Title">
-                    qw04011@gmail.com
-                </h2>
+                
+                <Main>Director: MurPan</Main>    
+                <a className="Title" href="mailto:qw04011@gmail.com">
+                <img 
+                 src={require("./img/GoogleLogo.png").default} 
+                 alt="Google"
+                 width='24'
+                 height='24'/>
+                </a>
+
             </div>
         </div>
     )
