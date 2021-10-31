c9eb6aa77588e05577544fbd57e7e1bd9cd099ec
murpan <qw04011@gmail.com>
2021-10-31 16:03:37 +0900

    todo.js add redux-actions

---
+++ src/modules/todos.js
@@ -0,0 +1,43 @@
+
+import { createAction, handleActions } from 'redux-actions'
+
+
+const CHANGE_INPUT = 'todos/CHANGE_INPUT';
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+export const changeInput = createAction(CHANGE_INPUT, input => input)
+let id = 3;
+
+export const insert = text => ({
+    id: id++,
+    text,
+    done: false
+});
+
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
+
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+        [INSERT]: (state, { payload: todo }) => ({
+            ...state,
+            todos: state.todos.concat(todo)
+        }),
+        [TOGGLE]: (state, { payload: id }) => ({
+            ...state,
+            todos: state.todos.map(todo =>
+                todo.id === id ? { ...todo, done: !todo.done } : todo)
+        }),
+        [REMOVE]: (state, { payload: id }) => ({
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== id),
+        })
+    },
+    initialState,
+
+)
+
+export default todos;
