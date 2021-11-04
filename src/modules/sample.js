ae7bed62e235e274126b75e56b8651491b35f4aa
murpan <qw04011@gmail.com>
2021-11-04 12:25:37 +0900

    APi code Refactoring

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -10,96 +10,28 @@ const GET_USERS = 'sample/GET_USER';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-export const getPost = id => async dispatch => {
-    dispatch({ type: GET_POST });
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        });
-    } catch (e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-        });
-        throw e;
-    }
-}
+export const getPost = createRequestThunk(GET_POST, api.getPost)
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers)
 
-export const getUsers = id => async dispatch => {
-    dispatch({ type: GET_USER });
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_USER_SUCCESS,
-            payload: response.data
-        });
-    } catch (e) {
-        dispatch({
-            type: GET_USER_FAILURE,
-            payload: e,
-            error: true
-        });
-        throw e;
-    }
-}
 
 const intialState = {
-    loading: {
-        GET_POST: false,
-        GET_USERS: false
-    },
+
     post: null,
     users: null
 };
 
 const sample = handleActions({
-    [GET_POST]: state => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_POST: true
-        }
-    }),
+
     [GET_POST_SUCCESS]: (state, action) => ({
         ...state,
-        loading: {
-            ...state.loading,
-            GET_POST: false
-        },
         post: action.payload
     }),
-    [GET_POST_FAILURE]: (state, action) => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_POST: false
-        },
-    }),
-    [GET_USERS]: state => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_USERS: true
-        }
-    }),
+
     [GET_USERS_SUCCESS]: (state, action) => ({
         ...state,
-        loading: {
-            ...state.loading,
-            GET_USERS: false
-        },
         users: action.payload
     }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-        ...state,
-        loading: {
-            ...state.loading,
-            GET_USERS: false
-        },
-    }),
+
 },
     intialState)
 
