b307a8aeb58cdde56eab89da98135ba9971ec180
murpan <qw04011@gmail.com>
2021-11-04 11:42:55 +0900

    add Sample.js API set

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,11 @@
 import { combineReducer } from 'redux';
 import counter from './counter';
+import sample from './sample';
 
 const rootReducer = combineReducer({
     counter,
+    sample,
+
 })
 
 export default rootReducer;
